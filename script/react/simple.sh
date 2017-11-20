edf733fe300a4a4487264ae6dd1031c2c1812109
Michael Lin <unadlib@gmail.com>
2017-11-21 00:51:07 +0800

    add react --easy

---
+++ script/react/simple.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+mkdir $1
+cd $1
+npm init -y
+echo '<div id="app"></div><script src="bundle.js"></script>' > index.html
+echo '{"presets": ["env","react"]}' > .babelrc
+cat <<EOF > index.js
+import React from 'react'
+import ReactDOM from 'react-dom'
+ReactDOM.render(<p>Hello, !</p>,document.getElementById('root'))
+EOF
+cat <<EOF > webpack.config.js
+module.exports = {
+  entry: "./index.js",
+  output: {
+    filename: "bundle.js",
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js|\.jsx/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      }
+    ]
+  }
+};
+EOF
+yarn add -D webpack webpack-dev-server babel-cli babel-preset-react babel-preset-env babel-loader
+yarn add react react-dom
